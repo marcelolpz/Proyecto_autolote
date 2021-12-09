65ea00757660a07cc15bd6669aeea16c74e5a2f6
marcelolpz <69664455+marcelolpz@users.noreply.github.com>
2021-12-09 06:49:54 -0600

    proyecto

---
+++ Proyecto_autolote/Proyecto_autolote/ModelsWebServices/Authenticator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_autolote.ModelsWebServices
+{
+
+
+    public class Item
+    {
+        public int validacion { get; set; }
+    }
+
+    public class Link
+    {
+        public string rel { get; set; }
+        public string href { get; set; }
+    }
+
+    public class Authenticator
+    {
+       
+        
+            public List<Item> items { get; set; }
+            public bool hasMore { get; set; }
+            public int limit { get; set; }
+            public int offset { get; set; }
+            public int count { get; set; }
+            public List<Link> links { get; set; }
+        
+
+    }
+}
---
+++ Proyecto_autolote/Proyecto_autolote/Viewmodel/VMMainPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_autolote.Viewmodel
+{
+     public class RestClient
+    {
+        private string weblinkUrl;
+
+        public async Task<T> validacionUsuario<T>(string p_username, string p_pass)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://apex.oracle.com/pls/apex/proyetoautolote/usuario/crud_usuario?id_usuario="
+                        + p_username + "&password="+ p_pass;
+            var response = await client.GetAsync(url);
+            var jsonstring = await response.Content.ReadAsStringAsync();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonstring);
+        }
+
+    }
+}
